a4afc51b85f192548c79442818c81bfae3da5b22
tatianasnook <tatsnook@gmail.com>
2024-06-29 15:55:40 -0500

    an assignment_1_3 is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import AddTodoForm from "./AddTodoForm.jsx";
 import TodoList from "./TodoList.jsx";
 
-
 function App() {
-  
+  const [newTodo, setNewTodo] = useState('');
+
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm />
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   )
