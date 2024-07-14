bcc47d43578e8441f4cd920b5784f87d96a46a19
tatianasnook <tatsnook@gmail.com>
2024-07-14 18:39:18 -0500

    assignment_1_4 is completed

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,18 +1,25 @@
-const AddTodoForm = (props) => {
+import { useState } from 'react';
+
+const AddTodoForm = ({ onAddTodo }) => {
+  const [todoTitle, setTodoTitle] = useState('');
+
+  const handleTitleChange = (e) => {
+    const newTodoTitle = e.target.value;
+    setTodoTitle(newTodoTitle);
+  }
 
   const handleAddTodo = (e) => {
     e.preventDefault();
-    let todoTitle = e.target.title.value;
     console.log(todoTitle);
-    e.target.reset();
-    props.onAddTodo(todoTitle);
+    setTodoTitle('');
+    onAddTodo({title: todoTitle, id: Date.now()});
   } 
 
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input id="todoTitle" name="title" />
-      <button type="submit">Add</button>
+      <input id="todoTitle" name="title" value={todoTitle} onChange={handleTitleChange} />
+      <button type="submit"className='add-btn'>Add</button>
     </form>
   )
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,6 @@
 import TodoListItem from "./TodoListItem";
 
-const todoList = [
-  {id: 1, title: 'Read material for the lesson'},
-  {id: 2, title: 'Do textbook exercises'},
-  {id: 3, title: 'Complete the assignment'}
-];
-
-const TodoList = () => {
+const TodoList = ({ todoList }) => {
   return (
     <ul>
       {todoList.map((todo) => (
